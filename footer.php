0dfc97d4f76a027b3849266c63359f0586d910fa
VladimirButko <vova_butko@inbox.ru>
2019-10-09 19:44:28 +0300

    Initial commit

---
+++ footer.php
@@ -0,0 +1,45 @@
+<footer>
+        <div class="wrapper">
+            <div class="container">
+                <div class="container-box">
+                    <div class="container-box-social">
+                        <ul class="">
+                            <li class="animate-show">
+                                <a href="hhtps://twitter.com/">
+                                    <i class="fa fa-twitter"></i>
+                                </a>
+                            </li>
+                            <li class="animate-show">
+                                <a href="https://www.facebook.com/">
+                                    <i class="fa fa-facebook"></i>
+                                </a>
+                            </li>
+                            <li class="animate-show">
+                                <a href="https://plus.google.com/">
+                                    <i class="fa fa-google-plus"></i>
+                                </a>
+                            </li>
+                            <li class="animate-show">
+                                <a href="instagram.com/">
+                                    <i class="fa fa-instagram"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <a href="#" class="scroll">
+                        <i class="pe-7s-up-arrow"></i>
+                    </a>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+    </footer>
+    </div>
+    <script src="jquery.js"></script>
+    <script src="script-jquery.js"></script>
+    <script src="script.js"></script>
+</body>
+</html>
